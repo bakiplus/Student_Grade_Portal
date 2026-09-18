dc1fad1fb10aacbd6cb99c83e83f94de8503c622
Isaac-Habesha <thegreatestbaki@gmail.com>
2026-09-18 21:07:38 +0300

    Configure WhiteNoise static files storage

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -144,6 +144,14 @@ USE_TZ = True
 # Static files (WhiteNoise for Production)
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
