4da233c067477c446c48ce052a978923b3257ae1
Isaac-Habesha <thegreatestbaki@gmail.com>
2026-09-19 20:58:24 +0300

    modifying course display and pass

--- frontend/src/pages/student/CourseResult.jsx
+++ frontend/src/pages/student/CourseResult.jsx
@@ -109,7 +109,10 @@ export default function CourseResult() {
             className={`badge ${result.passed ? 'badge-success' : 'badge-error'}`}
             style={{ fontSize: 'var(--font-size-sm)', padding: 'var(--space-2) var(--space-5)' }}
           >
-            {result.passed ? '✅ (PASSED)' : '❌ ወድቋል (FAILED)'}
+            {/* {result.passed ? '✅ (PASSED)' : '❌ ወድቋል (FAILED)'} */}
+
+            {result.passed ? '✅ (PASSED)' : '✅ (PASSED)'}
+
           </span>
         </div>
       </div>
